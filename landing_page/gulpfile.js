181ee2ea173c76ca873d228e04ae9ca48ee042d9
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2019-10-22 10:49:13 -0300

    Gulp changed to 3.9.1 version

--- landing_page/gulpfile.js
+++ landing_page/gulpfile.js
@@ -1,2 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const notify = require('gulp-notify');
+//const minify = require('gulp-jsmin');
+const concat = require('gulp-concat');
+
+gulp.task('default', ['sass', 'watch']);
+
+//task for sass
+gulp.task('sass', function(){
+    return gulp.src("src/assets/scss/style.scss")
+               .pipe(concat('style.min.css'))
+               .pipe(sass({outputStyle: "compressed"})
+               .on('error', notify.onError("Erro: <%= error.message %>")))
+               .pipe(gulp.dest('dist/assets/css/'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('src/assets/scss/style.scss', ['sass']);
+});
