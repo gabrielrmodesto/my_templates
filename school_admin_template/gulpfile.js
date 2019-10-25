6672413d7bb2cb7be84de4a77b026d868b096bae
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2019-10-25 11:46:32 -0300

    The first minified css, trying to automate the task

--- school_admin_template/gulpfile.js
+++ school_admin_template/gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var notify = require('gulp-notify');
+var concat = require('gulp-concat');
 
+gulp.task('default', gulp.series('sass','watch'));
 
 gulp.task('sass', function(){
     return gulp.src('src/assets/css/style.scss')
+               .pipe(concat('style.min.css'))
                .pipe(sass({outputStyle: "compressed"}))
                .on('error', notify.onError("Erro: <%= error.message %>"))
                .pipe(gulp.dest('dist/assets/css/'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('src/assets/scss/*.scss', ['sass']);
 });
