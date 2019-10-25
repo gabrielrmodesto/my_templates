2b644b5ce9a9893d9f07ab17b8a007f43705c196
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2019-10-25 12:48:13 -0300

    Gulp version changed, html minified, css minified

--- school_admin_template/gulpfile.js
+++ school_admin_template/gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var notify = require('gulp-notify');
 var concat = require('gulp-concat');
+const htmlmin = require('gulp-htmlmin');
 
-gulp.task('default', gulp.series('sass','watch'));
+gulp.task('default', ['sass','htmlmin','watch']);
 
+// task to minify css
 gulp.task('sass', function(){
     return gulp.src('src/assets/css/style.scss')
                .pipe(concat('style.min.css'))
@@ -13,6 +15,15 @@ gulp.task('sass', function(){
                .pipe(gulp.dest('dist/assets/css/'));
 });
 
+//task to minify html
+gulp.task('htmlmin', function(){
+    return gulp.src('src/*.html')
+               .pipe(htmlmin({collapseWhitespace: true}))
+               .on('error', notify.onError("Erro: <%= error.message %>"))
+               .pipe(gulp.dest('dist'));
+});
+
 gulp.task('watch', function(){
-    gulp.watch('src/assets/scss/*.scss', ['sass']);
+    gulp.watch('src/assets/css/*.scss', ['sass']);
+    gulp.watch('src/*.html', ['htmlmin']);
 });
