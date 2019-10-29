5a9bd8ef6c3c7fd741c28a68a0b161fd1c193f50
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2019-10-29 13:04:32 -0300

    I need to create interactive in login screen

--- school_admin_template/login-screen/src/pages/Login/Login.js
+++ school_admin_template/login-screen/src/pages/Login/Login.js
@@ -18,29 +18,45 @@ export default class Login extends Component {
                                     </div>
                                 </div>
                                 
-                                <form action="" className="login_page_form">
+                                <form onSubmit={handleFormLogin} className="login_page_form">
                                     <div className="row justify-content-center">
                                         <div className="form-group col-12 col-lg-10 col-md-10">
                                             <label htmlFor="loginEmail">E-mail</label>
-                                            <input type="email" name="loginEmail" id="loginEmail" placeholder="Enter with your e-mail" className="form-control input_login_form"/>
+                                            <input type="email" name="loginEmail" id="loginEmail" placeholder="Enter with your e-mail" className="form-control" value={email} onChange={event => }/>
                                         </div>
                                     </div>
                                     <div className="row justify-content-center">
                                         <div className="form-group col-12 col-lg-10 col-md-10">
                                             <label htmlFor="passwordLogin">Password</label>
-                                            <input type="password" name="passwordLogin" id="passwordLogin" placeholder="Enter with your password" className="form-control input_login_form"/>
+                                            <input type="password" name="passwordLogin" id="passwordLogin" placeholder="Enter with your password" className="form-control" value={password}/>
                                         </div>
                                     </div>
                                     <div className="row justify-content-center text-center">
-                                        <div className="col-10 form-group">
-                                            <button type="button" className="btn btn-primary btn-block">Enter</button>
+                                        <div className="col-12 col-lg-10 form-group">
+                                            <button type="button" className="btn btn-primary btn-block" id="loginBtn">Enter</button>
                                         </div>
                                     </div>
                                 </form>
+                                <div className="row justify-content-center text-center">
+                                    <div className="col-6 form-group">
+                                        <div class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" id="remember-me" />
+                                            <label class="custom-control-label" for="remember-me">Remember-me</label>
+                                        </div>
+                                    </div>
+                                    <div className="col-6 form-group">
+                                        <a href="" id="forgotpassword">Forgot Password?</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div> 
+                </div>
+                <div className="row text-center">
+                    <div className="col-12 form-group" id="signup">
+                        Don't have an account?<a href="" id="createaccount"> Create one</a>
+                    </div>
+                </div>
             </div>
         </div>  
     );
