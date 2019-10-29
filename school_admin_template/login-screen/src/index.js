facdbb4420159ceaae621a7c9ae657b28188203f
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2019-10-29 18:07:52 -0300

    The home screen created

--- school_admin_template/login-screen/src/index.js
+++ school_admin_template/login-screen/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Login from './pages/Login/Login';
+import Home from './pages/Home/index';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(<Login />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
