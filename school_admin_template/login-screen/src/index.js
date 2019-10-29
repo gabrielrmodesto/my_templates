2a750c9997602d2ea721386fa50d23e44754aeee
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2019-10-29 12:28:41 -0300

    Css ready

--- school_admin_template/login-screen/src/index.js
+++ school_admin_template/login-screen/src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Login from './pages/Login/Login';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './assets/css/main.css';
 
 ReactDOM.render(<Login />, document.getElementById('root'));
