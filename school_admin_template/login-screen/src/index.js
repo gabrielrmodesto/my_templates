2b647e8e90d60203c331d7a7cc0cef4e7ee793e1
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2019-10-31 11:10:46 -0300

    Some changes in public index

--- school_admin_template/login-screen/src/index.js
+++ school_admin_template/login-screen/src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Login from './pages/Login/Login';
 import Home from './pages/Home/index';
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(<Login />, document.getElementById('root'));
-ReactDOM.render(<Home />, document.getElementById('root'));
